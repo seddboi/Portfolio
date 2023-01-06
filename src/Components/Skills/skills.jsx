5c62aebc9b65ab96b3c75e83c7d1d035c7f223fc
Gian Zamora <gz2016@att.net>
2023-01-05 22:48:13 -0800

    adjusted skills section title animation and font

--- src/Components/Skills/skills.jsx
+++ src/Components/Skills/skills.jsx
@@ -1,10 +1,9 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Container, Box, Typography, ThemeProvider } from '@mui/material';
 import { theme } from '../../App';
 import './skills.css';
 
 export function Skills() {
-
 	const observer = new IntersectionObserver((entries) => {
 		entries.forEach((entry) => {
 			const skillTitles = document.querySelectorAll('.title');
@@ -25,13 +24,17 @@ export function Skills() {
 		<ThemeProvider theme={theme}>
 			<Container disableGutters>
 				<Box className='title-wrapper' sx={{display: 'flex', flexDirection: 'row'}}>
-					<Typography className='title' sx={{color: 'var(--mango)', fontFamily: "'3dlet'", lineHeight: '0.9em', fontSize: {md: '150px', sm: '130px',  xs: '75px'}}}>s</Typography>
-					<Typography className='title' sx={{color: 'var(--mango)', fontFamily: "'3dlet'", lineHeight: '0.9em', fontSize: {md: '150px', sm: '130px',  xs: '75px'}}}>k</Typography>
-					<Typography className='title' sx={{color: 'var(--mango)', fontFamily: "'3dlet'", lineHeight: '0.9em', fontSize: {md: '150px', sm: '130px',  xs: '75px'}}}>i</Typography>
-					<Typography className='title' sx={{color: 'var(--mango)', fontFamily: "'3dlet'", lineHeight: '0.9em', fontSize: {md: '150px', sm: '130px',  xs: '75px'}}}>l</Typography>
-					<Typography className='title' sx={{color: 'var(--mango)', fontFamily: "'3dlet'", lineHeight: '0.9em', fontSize: {md: '150px', sm: '130px',  xs: '75px'}}}>l</Typography>
-					<Typography className='title' sx={{color: 'var(--mango)', fontFamily: "'3dlet'", lineHeight: '0.9em', fontSize: {md: '150px', sm: '130px',  xs: '75px'}}}>s</Typography> 
-				</Box>				
+					<Typography className='title' sx={{color: 'var(--mango)', fontFamily: "'Bronson'", lineHeight: '0.9em', fontSize: {md: '130px', sm: '100px',  xs: '65px'}, textShadow: '0 3px 3px #000'}}>S</Typography>
+					<Typography className='title' sx={{color: 'var(--mango)', fontFamily: "'Bronson'", lineHeight: '0.9em', fontSize: {md: '130px', sm: '100px',  xs: '65px'}, textShadow: '0 3px 3px #000'}}>k</Typography>
+					<Typography className='title' sx={{color: 'var(--mango)', fontFamily: "'Bronson'", lineHeight: '0.9em', fontSize: {md: '130px', sm: '100px',  xs: '65px'}, textShadow: '0 3px 3px #000'}}>i</Typography>
+					<Typography className='title' sx={{color: 'var(--mango)', fontFamily: "'Bronson'", lineHeight: '0.9em', fontSize: {md: '130px', sm: '100px',  xs: '65px'}, textShadow: '0 3px 3px #000'}}>l</Typography>
+					<Typography className='title' sx={{color: 'var(--mango)', fontFamily: "'Bronson'", lineHeight: '0.9em', fontSize: {md: '130px', sm: '100px',  xs: '65px'}, textShadow: '0 3px 3px #000'}}>l</Typography>
+					<Typography className='title' sx={{color: 'var(--mango)', fontFamily: "'Bronson'", lineHeight: '0.9em', fontSize: {md: '130px', sm: '100px',  xs: '65px'}, textShadow: '0 3px 3px #000'}}>s</Typography> 
+				</Box>	
+				<Box className='skills-wrapper'>
+					<Box className='sliders-svg-wrapper' sx={{mt: 14, mb: 14, width: {md: '600px', sm: '350px'}}}>
+					</Box>
+				</Box>			
 			</Container>
 		</ThemeProvider>
 	)
