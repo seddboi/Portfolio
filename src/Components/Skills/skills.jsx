2f7a08c9ee07e26f17a410e95e42d73bf9601cbd
Gian Zamora <gz2016@att.net>
2023-01-06 18:48:15 -0800

    adjusted env variables for server

--- src/Components/Skills/skills.jsx
+++ src/Components/Skills/skills.jsx
@@ -24,15 +24,16 @@ export function Skills() {
 		<ThemeProvider theme={theme}>
 			<Container disableGutters>
 				<Box className='title-wrapper' sx={{display: 'flex', flexDirection: 'row'}}>
-					<Typography className='title' sx={{color: 'var(--mango)', fontFamily: "'Bronson'", lineHeight: '0.9em', fontSize: {md: '130px', sm: '100px',  xs: '65px'}, textShadow: '0 3px 3px #000'}}>S</Typography>
-					<Typography className='title' sx={{color: 'var(--mango)', fontFamily: "'Bronson'", lineHeight: '0.9em', fontSize: {md: '130px', sm: '100px',  xs: '65px'}, textShadow: '0 3px 3px #000'}}>k</Typography>
-					<Typography className='title' sx={{color: 'var(--mango)', fontFamily: "'Bronson'", lineHeight: '0.9em', fontSize: {md: '130px', sm: '100px',  xs: '65px'}, textShadow: '0 3px 3px #000'}}>i</Typography>
-					<Typography className='title' sx={{color: 'var(--mango)', fontFamily: "'Bronson'", lineHeight: '0.9em', fontSize: {md: '130px', sm: '100px',  xs: '65px'}, textShadow: '0 3px 3px #000'}}>l</Typography>
-					<Typography className='title' sx={{color: 'var(--mango)', fontFamily: "'Bronson'", lineHeight: '0.9em', fontSize: {md: '130px', sm: '100px',  xs: '65px'}, textShadow: '0 3px 3px #000'}}>l</Typography>
-					<Typography className='title' sx={{color: 'var(--mango)', fontFamily: "'Bronson'", lineHeight: '0.9em', fontSize: {md: '130px', sm: '100px',  xs: '65px'}, textShadow: '0 3px 3px #000'}}>s</Typography> 
+					<Typography className='title' sx={{fontSize: {md: '130px', sm: '100px',  xs: '65px'}}}>S</Typography>
+					<Typography className='title' sx={{fontSize: {md: '130px', sm: '100px',  xs: '65px'}}}>k</Typography>
+					<Typography className='title' sx={{fontSize: {md: '130px', sm: '100px',  xs: '65px'}}}>i</Typography>
+					<Typography className='title' sx={{fontSize: {md: '130px', sm: '100px',  xs: '65px'}}}>l</Typography>
+					<Typography className='title' sx={{fontSize: {md: '130px', sm: '100px',  xs: '65px'}}}>l</Typography>
+					<Typography className='title' sx={{fontSize: {md: '130px', sm: '100px',  xs: '65px'}}}>s</Typography> 
 				</Box>	
 				<Box className='skills-wrapper'>
 					<Box className='sliders-svg-wrapper' sx={{mt: 14, mb: 14, width: {md: '600px', sm: '350px'}}}>
+					
 					</Box>
 				</Box>			
 			</Container>
