1c6982a9886ade45e367cfec4083644f122399a8
Gian Zamora <gz2016@att.net>
2023-02-04 19:04:01 -0800

    added animation to contact me section

--- src/Components/Contact/contact.jsx
+++ src/Components/Contact/contact.jsx
@@ -1,8 +1,18 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Container, Box, ThemeProvider, Typography, Stack, Card, CardContent, Divider, Button } from '@mui/material';
+import { useInView } from 'react-intersection-observer';
 import './contact.css';
 
 export function Contact() {
+	const { ref: contactme, inView: isContactTitleVisible } = useInView();
+
+	useEffect(() => {
+		if (isContactTitleVisible) {
+			document.querySelectorAll('.title-element').forEach((entry) => {
+				entry.classList.add('title-animation');
+			});
+		}
+	}, [isContactTitleVisible]);
 	return (
 		<ThemeProvider>
 			<Container
@@ -12,72 +22,68 @@ export function Contact() {
 			>
 				<Box
 					className="contact-me-title"
+					ref={contactme}
 					sx={{ display: 'flex', flexDirection: 'row', position: 'sticky', top: '20px', mb: 22, zIndex: 2 }}
 				>
 					<Typography
 						variant="p"
-						className="title-letter"
+						className="title-element"
 						sx={{ userSelect: 'none', fontSize: { xs: '11vw', sm: '9vw', md: '8vw' } }}
 					>
 						C
 					</Typography>
 					<Typography
 						variant="p"
-						className="title-letter"
+						className="title-element"
 						sx={{ userSelect: 'none', fontSize: { xs: '11vw', sm: '9vw', md: '8vw' } }}
 					>
 						o
 					</Typography>
 					<Typography
 						variant="p"
-						className="title-letter"
+						className="title-element"
 						sx={{ userSelect: 'none', fontSize: { xs: '11vw', sm: '9vw', md: '8vw' } }}
 					>
 						n
 					</Typography>
 					<Typography
 						variant="p"
-						className="title-letter"
+						className="title-element"
 						sx={{ userSelect: 'none', fontSize: { xs: '11vw', sm: '9vw', md: '8vw' } }}
 					>
 						t
 					</Typography>
 					<Typography
 						variant="p"
-						className="title-letter"
+						className="title-element"
 						sx={{ userSelect: 'none', fontSize: { xs: '11vw', sm: '9vw', md: '8vw' } }}
 					>
 						a
 					</Typography>
 					<Typography
 						variant="p"
-						className="title-letter"
+						className="title-element"
 						sx={{ userSelect: 'none', fontSize: { xs: '11vw', sm: '9vw', md: '8vw' } }}
 					>
 						c
 					</Typography>
 					<Typography
 						variant="p"
-						className="title-letter"
+						className="title-element"
 						sx={{ userSelect: 'none', fontSize: { xs: '11vw', sm: '9vw', md: '8vw' } }}
 					>
 						t
 					</Typography>
 					<Typography
 						variant="p"
-						className="title-letter"
-						sx={{ userSelect: 'none', fontSize: { xs: '11vw', sm: '9vw', md: '8vw' } }}
-					></Typography>
-					<Typography
-						variant="p"
-						className="title-letter"
+						className="title-element"
 						sx={{ userSelect: 'none', fontSize: { xs: '11vw', sm: '9vw', md: '8vw' } }}
 					>
 						M
 					</Typography>
 					<Typography
 						variant="p"
-						className="title-letter"
+						className="title-element"
 						sx={{ userSelect: 'none', fontSize: { xs: '11vw', sm: '9vw', md: '8vw' } }}
 					>
 						e
